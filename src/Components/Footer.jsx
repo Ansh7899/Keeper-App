9b0ca3f2d7d14a719e5e0ef5d3f25fdfe1f37957
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-03-11 01:36:44 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const date = new Date().getFullYear();
+
+function Footer() {
+  return ( <
+    footer >
+    <
+    p > CopyrightⒸ {
+      date
+    } <
+    /p> <
+    /footer>);
+  }
+
+  export default Footer;
---
+++ src/Components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Note(){
+  return <div class="note">
+  <h1>TITLE</h1>
+  <p>content</p>
+  </div>
+}
+
+export default Note;
